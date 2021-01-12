9464275baa6e403ff1e332e86a08da3ecde9b48b
elliefh <elliefu18@gmail.com>
2021-01-12 14:21:10 -0800

    setting up project with html, css, and js file

---
+++ script.js
@@ -0,0 +1,13 @@
+// Get current time and date from Moment.js
+
+// Use the For Loop to determine if current time has past certain hours of the workday; passed hours will be colored gray 
+// var i = 0;
+// Element ID = i;
+// Check 9am < current hour
+// 9am is in the past 
+// Select matching element - $(Element ID).css("background");
+
+// Add a click event on a parent element that can listen to my save buttons clicks
+
+//
+
