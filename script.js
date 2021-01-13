f56b328931d507bc0bcddacd037684262437e337
elliefh <elliefu18@gmail.com>
2021-01-12 21:00:51 -0800

    created a for loop to determine current time, future time, and past time

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
 // Get current date and hour from Moment.js
 var date = moment().format('MMMM Do YYYY');
 var hour = moment().format('HH');
-var today = moment();
 console.log(date);
 console.log(hour);
 
 // Print current date 
 $("#currentDay").text(date);
 
-// Set up timeblocks
+// For Loop creates a row for each working hour between 9am-5pm; the row contains 1) the hour, 2) text area to input to-do items, and 3) button to save the user's inputs 
 var container = $('.container');
-
 for (let i = 9; i < 18; i++) {
     var row = $('<div>');
-    row.attr('id',i);
+    row.attr('class','row');
+    
+    var workHour = $('<div>');
+    workHour.attr('class', 'hour col-2');
+    // If else statements ensure that the working hour is printed in standard (12 hour clock) time
+    if (i<12) {
+        workHour.text(i+" am");
+    } 
+    else if (i>12) {
+        workHour.text(i-12 + " pm");
+    }
+    else {
+        workHour.text(i+" pm");
+    }
+    
+    var workPlan = $('<textarea>');
+    workPlan.attr('class', 'workPlan col-8');
+    workPlan.attr('id', i);
+
+    var saveButton = $('<div>');
+    saveButton.attr('class', 'saveBtn col-2');
+
+    // append created elements to the correct parent 
     container.append(row);
+    row.append(workHour, workPlan, saveButton);
 }
 
-// Use the For Loop to determine if current time has past certain hours of the workday; passed hours will be colored gray 
-// var i = 0;
-// Element ID = i;
-// Check 9am < current hour
-// 9am is in the past 
-// Select matching element - $(Element ID).css("background");
+// For Loop determines current time, future time, and past time and assigns a color 
+for (let i = 9; i < 18; i++) {
+    var j = i.toString();
+    if (i<hour) {
+        $('#'+j).addClass('past');
+    }
+    else if (i=hour) {
+        $('#'+j).addClass('present');
+    }
+    else {
+        $('#'+j).addClass('future');
+    }
+}
 
 // Add a click event on a parent element that can listen to my save buttons clicks
 
